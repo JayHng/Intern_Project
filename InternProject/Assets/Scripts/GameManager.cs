990033c9bb539a924122255eccb34774b1852bec
Jay <mhanhxv@gmail.com>
2020-05-07 14:11:15 -0400

    PauseMenu
    Add the pause menu in the main scene

---
+++ InternProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    public void PlayGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene("TitleScreen");
+
+    }
+}
