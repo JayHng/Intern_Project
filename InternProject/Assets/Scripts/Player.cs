6dbaa484f78b8efba27038f0c3a985298be044a9
Jay <mhanhxv@gmail.com>
2020-05-07 22:34:59 -0400

    Update PlayerUI 1
    Add HeartUI

--- InternProject/Assets/Scripts/Player.cs
+++ InternProject/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //This code belongs to Sebastian Lague
 [RequireComponent (typeof(Controller2D))]
@@ -17,18 +18,28 @@ public class Player : MonoBehaviour
     float velocityXSmoothing;
     Controller2D controller;
     Vector3 velocity;
+
+    //This code belongs to me
+    public int currentHP;
+    public int maxHP = 5;
+
     // Start is called before the first frame update
     void Start()
     {
+        //This code belongs to Sebastian Lague
         controller = GetComponent<Controller2D>();
         gravity = -(2*jumpHeight)/Mathf.Pow(timeToJumpApex,2);
         jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
         print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+
+        //This code belongs to me
+        currentHP = maxHP;
     }
     // Update is called once per frame
     void Update()
     {
-        if(controller.objectCol.above || controller.objectCol.below){
+        //This code belongs to Sebastian Lague
+        if (controller.objectCol.above || controller.objectCol.below){
             velocity.y = 0;
         }
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -42,8 +53,19 @@ public class Player : MonoBehaviour
         //update gravity to the velocity
         velocity.y += gravity * Time.deltaTime;
 
-        controller.Move(velocity * Time.deltaTime);
-        
+        controller.Move(velocity * Time.deltaTime);    
     }
 
+    //This code belongs to me
+    private void FixedUpdate()
+    {
+        if (currentHP <= 0)
+        {
+            Death();
+        }
+    }
+    public void Death()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
