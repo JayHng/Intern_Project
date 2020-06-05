7ae6969e6d4bc177aa3f4738ddc0f3be170856d1
Jay <mhanhxv@gmail.com>
2020-06-05 19:39:08 -0400

    Update Physics Material 2D in enemy

--- InternProject/Assets/Scripts/Enemy/States/Data/D_MoveState.cs
+++ InternProject/Assets/Scripts/Enemy/States/Data/D_MoveState.cs
@@ -6,5 +6,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "newMoveStateData", menuName = "Data/State Data/Move State")]
 public class D_MoveState : ScriptableObject
 {
-    public float movementSpeed = 3.0f; 
+    public float movementSpeed = 2.0f; 
 }
