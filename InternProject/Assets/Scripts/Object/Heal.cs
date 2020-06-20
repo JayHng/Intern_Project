53fdcb8a73eefb8f8f3028df3e72beb84bcd211c
Jay <mhanhxv@gmail.com>
2020-06-19 22:22:36 -0400

    Add items + Fix Load Scene
    Add Heart (Healing)

---
+++ InternProject/Assets/Scripts/Object/Heal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.tag == "Player")
+        {
+            Destroy(gameObject);
+            player.currentHP += 1;
+        }
+    }
+    
+}
