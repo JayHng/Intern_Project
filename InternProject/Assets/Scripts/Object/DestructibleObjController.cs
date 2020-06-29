3d1251d6e1ba84c05a676fee52e94037f6259efc
Jay <mhanhxv@gmail.com>
2020-06-28 20:15:30 -0400

    Destructible Object Part 1

---
+++ InternProject/Assets/Scripts/Object/DestructibleObjController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObjController : MonoBehaviour
+{
+    [SerializeField] private float maxHP;
+    private float currentHP;
+    private Player player;
+    private GameObject aliveGO, brokenTopGO, brokenBotGO;
+    private Rigidbody2D rbAlive, rbBrokenTop, rbBrokenBot;
+    private Animator aliveAnim;
+
+    private void Start() {
+        currentHP =  maxHP;
+
+        player = GameObject.Find("Player").GetComponent<Player>();
+        aliveGO = transform.Find("Vase").gameObject;
+        brokenTopGO = transform.Find("Broken Top").gameObject;
+        brokenBotGO = transform.Find("Broken Bottom").gameObject;
+
+        aliveAnim = aliveGO.GetComponent<Animator>();
+        rbAlive = aliveGO.GetComponent<Rigidbody2D>();
+        rbBrokenTop = brokenTopGO.GetComponent<Rigidbody2D>();
+        rbBrokenBot = brokenBotGO.GetComponent<Rigidbody2D>();
+
+        aliveGO.SetActive(true);
+        brokenTopGO.SetActive(false);
+        brokenBotGO.SetActive(false);
+    }
+
+    private void Damage(float amount){
+        currentHP -= amount;
+    }
+}
